1c2354ccb37c7f92c8c9a7eb8880cc50ff663c8d
Mario <marioperozo@gmail.com>
2020-04-07 22:13:34 -0400

    Show Picture

--- sliderhomework/ViewController.swift
+++ sliderhomework/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var picture: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        picture.image = UIImage(named: "squares")
+        picture.alpha = 0.0;
+
     }
-
-
+    
+    @IBAction func slider(_ sender: UISlider) {
+        
+        picture.alpha = CGFloat(sender.value);
+    }
+    
 }
+
 
