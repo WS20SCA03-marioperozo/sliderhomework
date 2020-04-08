b60c8f8089d8b2f2e97d5e0bea6d6765cd794203
Mario <marioperozo@gmail.com>
2020-04-07 21:12:45 -0400

    Initial Commit

---
+++ sliderhomework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sliderhomework
+//
+//  Created by Mario Perozo on 4/7/20.
+//  Copyright © 2020 Mario Perozo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
